1a1dae23969624d6f2eea8a5f5e0dbf293e1db58
grndstt <pierre-marie.allard@unige.ch>
2021-10-23 08:05:00 +0200

    step to remove previously fragged entries

--- scripts/frag_list_preparator_nb.py
+++ scripts/frag_list_preparator_nb.py
@@ -11,18 +11,27 @@ import sys
 
 
 
-metadata_file_path = '/Users/pma/01_large_files/lotus/wetransfer_lotus_2021-10-15_0844/platinum.tsv.gz'
+metadata_file_path = '~/cfm/platinum.tsv.gz'
+fragged_list_path = '~/cfm/calculated_spectra.txt'
 sep = '\t'
 duplicated_field_header = 'structureCleaned_inchikey2D'
 id_header = 'structureCleaned_inchikey2D'
 smiles_header = 'structureCleaned_smiles2D'
-for_frag_file_path = '/Users/pma/01_large_files/lotus/wetransfer_lotus_2021-10-15_0844/platinum_tofrag.tsv.gz'
+for_frag_file_path = '~/cfm/cfm_input/sub_platinum_tofrag.tsv.gz'
 
 
 
 
 df = pd.read_csv(metadata_file_path, sep=sep, error_bad_lines=False, low_memory=False)
 df = pd.read_csv(metadata_file_path, compression='gzip', header=0, sep=sep, quotechar='"', error_bad_lines=False)
+
+
+fragged_list = pd.read_csv(fragged_list_path,header = None, quotechar='"', error_bad_lines=False)
+
+
+fragged_list[0]
+type(fragged_list[0])
+
 
 df.columns
 
@@ -34,6 +43,10 @@ df.info()
 
 
 df = df[[id_header, smiles_header]]
+
+# here we keep all entries which are not in the previously fragmented list 
+
+df[~df[id_header].isin(fragged_list[0])]
 
 
 df.to_csv(for_frag_file_path, sep=' ', index=False, compression='gzip', header=None)
